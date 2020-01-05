2cf5aa9084de44da8c0293f11202211b4e07a938
rakeshpandey <rakeshpandey791@gmail.com>
2020-01-05 12:02:53 +0530

    Updates

---
+++ precache-manifest.d873ad947c0deca05eccdd2fc7e3a785.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a88d760de0aeeff7486e633b80a2fa2",
+    "url": "/mac-calculator/index.html"
+  },
+  {
+    "revision": "e113f4f6f4021a781725",
+    "url": "/mac-calculator/static/css/main.5147d049.chunk.css"
+  },
+  {
+    "revision": "bfb21e5924f8be78cd7c",
+    "url": "/mac-calculator/static/js/2.94c28c72.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mac-calculator/static/js/2.94c28c72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e113f4f6f4021a781725",
+    "url": "/mac-calculator/static/js/main.c119c0d0.chunk.js"
+  },
+  {
+    "revision": "a45f02b859c4be7f1c01",
+    "url": "/mac-calculator/static/js/runtime-main.a629c238.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mac-calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
